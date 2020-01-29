8ea55992352236f5205cbd199dbc631bf1d61a95
IB_Student <student@InnovateBhams-MacBook-Pro-4.local>
2020-01-29 10:58:38 -0600

    m

--- DOM/script.js
+++ DOM/script.js
@@ -42,47 +42,41 @@ document.addEventListener('DOMContentLoaded', function () {
             '#E6B333', '#3366E6', '#999966'];
         div.style.color = colorArray[Math.floor(Math.random() * colorArray.length)]
     }
+
+
+    //create div and button to span my name
+    let button = document.createElement('button');
+    let btnTxt = document.createTextNode('Click!');
+    button.appendChild(btnTxt);
+    document.body.appendChild(button);
+
+    button.addEventListener("click", function () {
+        let span = document.createElement('span');
+        let spanText = document.createTextNode("Hallie ");
+
+        span.appendChild(spanText);
+        document.body.appendChild(span);
+    })
+
     
+    //list of friends when clicked
+    let ul = document.getElementById('ul');
+    let list = document.getElementById('list');
+    let count = 0
+    list.addEventListener('click', function () {
 
-//create div and button to span my name
-let button = document.createElement('button');
-let btnTxt = document.createTextNode('Click!');
-button.appendChild(btnTxt);
-document.body.appendChild(button);
+        let friends = ["Brittany", "Mallory", "Rachel", "TJ", "Frank", "Candace", "Chase", "Chris", "Aaron", "Brooke"];
 
-button.addEventListener("click", function (){
-    let span = document.createElement('span');
-    let spanText = document.createTextNode("Hallie ");
+        if (count < friends.length - 1) {
 
-    span.appendChild(spanText);
-    document.body.appendChild(span);
-})
+            let li = document.createElement('li');
+            li.appendChild(document.createTextNode(friends[count]));
+            ul.appendChild(li)
+            count++
+        }
 
 
-let ul = document.getElementById('ul');
-let list = document.getElementById('list');
-let count = 0
-list.addEventListener('click', function (){
-
-    let friends = ["Brittany", "Mallory", "Rachel", "TJ", "Frank", "Candace", "Chase", "Chris", "Aaron", "Brooke"];
-
-    if(count < friends.length - 1) { 
-    
-let li = document.createElement('li');
-li.appendChild(document.createTextNode(friends[count]));
-ul.appendChild(li)
-    count++
     }
+    )
 
-
-}
-
-
-)
 })
-
-
-
-
-
-
--- JQuery/app.js
+++ JQuery/app.js
@@ -1,7 +1,23 @@
+// button with a nice alert 
+$('#button').on("click", function () {
+    alert("Hello!");
+});
+
+// form with alert set to what is typed
+$('#buttonSubmit').on("click", function () {
+    alert($("#someText").val())
+});
 
 
-$(document).ready(function(){
-    $('#submit').click(function(){
-        alert("hi! :)");
+// div change background color
+$(function() {
+    $('div').hover(
+    function() {
+        $(this).css({
+            'background-color': '#b3b3ff'})
+    }, function() {
+        $(this).css('background-color', '')
     });
 });
+
+//text paragraph changes to random color when clicked
